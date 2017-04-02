36bbda5ad287c83f2a313e37f3b67c64e1c05d35
Protsiv Andrey(home) <protsiv.a.wezmo@gmail.com>
2017-04-02 22:59:51 +0300

    Create posts CRUD

---
+++ backend/models/PostsSearch.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use backend\models\Posts;
+
+/**
+ * PostsSearch represents the model behind the search form about `backend\models\Posts`.
+ */
+class PostsSearch extends Posts
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'published', 'category_id'], 'integer'],
+            [['name', 'slug', 'description', 'anounce', 'published_at', 'tags'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Posts::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'published' => $this->published,
+            'published_at' => $this->published_at,
+            'category_id' => $this->category_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'name', $this->name])
+            ->andFilterWhere(['like', 'slug', $this->slug])
+            ->andFilterWhere(['like', 'description', $this->description])
+            ->andFilterWhere(['like', 'anounce', $this->anounce])
+            ->andFilterWhere(['like', 'tags', $this->tags]);
+
+        return $dataProvider;
+    }
+}
---
+++ backend/views/posts/_form.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Posts */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="posts-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'slug')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'anounce')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'published')->textInput() ?>
+
+    <?= $form->field($model, 'published_at')->textInput() ?>
+
+    <?= $form->field($model, 'category_id')->textInput() ?>
+
+    <?= $form->field($model, 'tags')->textarea(['rows' => 6]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
